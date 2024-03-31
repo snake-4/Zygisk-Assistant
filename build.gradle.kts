0ee57d586f81b95b0f2e053b13b9855ca4fbde00
snake-4 <18491360+snake-4@users.noreply.github.com>
2024-03-31 23:15:32 +0200

    Added updateJson in module.prop, bumped to v2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,6 @@ val commitHash: String by extra({
 
 val moduleId by extra("zygisk-assistant")
 val moduleName by extra("Zygisk Assistant")
-val verName by extra("v2.0.0")
-val verCode by extra(200)
+val verName by extra("v2.0.1")
+val verCode by extra(201)
 val abiList by extra(listOf("armeabi-v7a","arm64-v8a","x86","x86_64"))
---
+++ update_metadata/CHANGELOG.md
@@ -0,0 +1,2 @@
+## 2.0.1
++ Added update.json for automatic updates.
