71eee6bf9256e64b1386c48b6162564a4b33594c
snake-4 <18491360+snake-4@users.noreply.github.com>
2024-04-24 07:01:50 +0200

    Added ELFIO submodule

--- module/jni/Android.mk
+++ module/jni/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/elfio
 LOCAL_MODULE := zygisk
 LOCAL_SRC_FILES := utils.cpp map_parser.cpp mount_parser.cpp mountinfo_parser.cpp unmount.cpp main.cpp
 LOCAL_STATIC_LIBRARIES := libcxx
