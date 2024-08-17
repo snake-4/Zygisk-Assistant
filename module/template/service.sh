b3767f77c9c7aa0aba4f97bdcaf28fc16e451a21
snake-4 <18491360+snake-4@users.noreply.github.com>
2024-08-17 16:22:37 +0200

    Added new props to service.sh

--- module/template/service.sh
+++ module/template/service.sh
@@ -1,49 +1,51 @@
 MODPATH="${0%/*}"
 . $MODPATH/common_func.sh
 
-# Conditional sensitive properties
-
-# Magisk Recovery Mode
+# Recovery Mode
 resetprop_if_match ro.boot.mode recovery unknown
 resetprop_if_match ro.bootmode recovery unknown
 resetprop_if_match vendor.boot.mode recovery unknown
 
 # SELinux
 resetprop_if_diff ro.boot.selinux enforcing
-# use delete since it can be 0 or 1 for enforcing depending on OEM
 if [ -n "$(resetprop ro.build.selinux)" ]; then
     resetprop --delete ro.build.selinux
 fi
-# use toybox to protect stat access time reading
+
+# SELinux file access time
 if [ "$(toybox cat /sys/fs/selinux/enforce)" = "0" ]; then
     chmod 640 /sys/fs/selinux/enforce
     chmod 440 /sys/fs/selinux/policy
 fi
 
-# Conditional late sensitive properties
-
-# must be set after boot_completed for various OEMs
 {
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
     sleep 1
 done
 
-# SafetyNet/Play Integrity + OEM
-# avoid bootloop on some Xiaomi devices
+# Avoid bootloop on some Xiaomi devices
 resetprop_if_diff ro.secureboot.lockstate locked
-# avoid breaking Realme fingerprint scanners
+
+# Avoid breaking Realme fingerprint scanners
 resetprop_if_diff ro.boot.flash.locked 1
 resetprop_if_diff ro.boot.realme.lockstate 1
-# avoid breaking Oppo fingerprint scanners
+
+# Avoid breaking Oppo fingerprint scanners
 resetprop_if_diff ro.boot.vbmeta.device_state locked
-# avoid breaking OnePlus display modes/fingerprint scanners
+
+# Avoid breaking OnePlus display modes/fingerprint scanners
 resetprop_if_diff vendor.boot.verifiedbootstate green
-# avoid breaking OnePlus/Oppo fingerprint scanners on OOS/ColorOS 12+
+
+# Avoid breaking OnePlus/Oppo fingerprint scanners on OOS/ColorOS 12+
 resetprop_if_diff ro.boot.verifiedbootstate green
 resetprop_if_diff ro.boot.veritymode enforcing
 resetprop_if_diff vendor.boot.vbmeta.device_state locked
 
 # Other
 resetprop_if_diff sys.oem_unlock_allowed 0
+resetprop_if_diff ro.secure 1
+resetprop_if_diff ro.adb.secure 1
+resetprop_if_diff ro.build.type user
+resetprop_if_diff ro.build.tags release-keys
 
 }&
